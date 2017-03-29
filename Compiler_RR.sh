97b5b8f5020004f4d4ee374b38819944b24efcc0
Gr2_srk <gvjohan99@gmail.com>
2017-03-29 17:13:11 -0600

    update :v

--- Compiler_RR.sh
+++ Compiler_RR.sh
@@ -6,40 +6,34 @@
 #
 
 #!/bin/bash
+echo ' ' 
+echo ' '
+echo ' Entrando al directorio'
+echo ' ' 
+echo ' '
+cd RR 
 
-#Inicio
-echo ''
-echo "======================"
-echo ''
-echo " DemoniacTeamCompiler"
-echo '     Version RR       '
-echo ''
-echo "======================"
-echo ''
+rm -rf ~/.jack*
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
+./prebuilts/sdk/tools/jack-admin kill-server
+./prebuilts/sdk/tools/jack-admin start-server
+rm -rvf ../.ccache
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
 
-#SOURCE
-#Tres descargas por que luego falla :v
-cd 'RR LP' 
-repo sync -j16 -f --force-sync --no-clone-bundle
-repo sync -j16 -f --force-sync --no-clone-bundle
+echo ' ' 
+echo ' '
+echo ' Iniciando compilación'
+echo ' ' 
+echo ' '
+echo ' Estableciendo tiempo de apagado en 4 horas'
+sudo shutdown -h +240
+
 repo sync -j16 -f --force-sync --no-clone-bundle
 
 #LocalManifest
-git clone https://github.com/DemoniacTeamDevs/local_manifests_hwY635 -b master .repo/local_manifests
+git clone https://github.com/gr2007/Local_manifest -b master .repo/local_manifests
 repo sync --force-sync --force-broken
 
 #Compilacion
-. build/envsetup.sh
-brunch hwY635
-
-#TriunfoElMal
-echo "======================"
-echo ' ozy papu soy la ostia compile RR sin ti'
-echo 'Tengo el poder :v'
-echo 'alv'
-echo 'pa que sirves'
-echo 'mejor yo lo hago sola'
-echo ':v'
-echo 'Gr2 es el Papu' 
-echo 'GGGGGG'
-echo "======================"
+. build/envsetup.sh && brunch hwY635
+ 
