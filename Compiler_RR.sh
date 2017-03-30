5c56cffffc90bd696698b34343bd76713ce4e03b
Jose Angel ü Asií Nomas <ghettoxx2@gmail.com>
2017-03-29 18:15:14 -0600

    Update Compiler_RR.sh

--- Compiler_RR.sh
+++ Compiler_RR.sh
@@ -11,7 +11,7 @@ echo ' '
 echo ' Entrando al directorio'
 echo ' ' 
 echo ' '
-cd RR 
+cd aosgp
 
 rm -rf ~/.jack*
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
@@ -26,13 +26,9 @@ echo ' Iniciando compilación'
 echo ' ' 
 echo ' '
 echo ' Estableciendo tiempo de apagado en 4 horas'
-sudo shutdown -h +240
 
 repo sync -j16 -f --force-sync --no-clone-bundle
 
-#LocalManifest
-git clone https://github.com/gr2007/Local_manifest -b master .repo/local_manifests
-repo sync --force-sync --force-broken
 
 #Compilacion
 . build/envsetup.sh && brunch hwY635
