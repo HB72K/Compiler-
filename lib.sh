e02964af556ba546871acb5c1391c3463fc148f8
Jose Angel ü Asií Nomas <ghettoxx2@gmail.com>
2017-04-08 11:37:53 -0500

    minor fix :v

--- lib.sh
+++ lib.sh
@@ -52,7 +52,7 @@ sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update && sudo apt-get install openjdk-8-jdk -s
 
 
-udo apt-get update && sudo apt-get install git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib maven tmux screen w3m ncftp -s
+sudo apt-get update && sudo apt-get install git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib maven tmux screen w3m ncftp -s
 
 
 printf "${purple}Developer HB72K :v ...${nc}\n"
